7aac55481c42b2c1a01c89b6fca72f4f82c03f97
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-16 09:08:58 +0200

    add UserController, add ProfileModel

---
+++ PrivacyPulse-BACK/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using CampingAPI.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrivacyPulse_BACK.Attributes;
+using PrivacyPulse_BACK.Models;
+
+namespace PrivacyPulse_BACK.Controllers
+{
+    [Route("api/[controller]")]
+    [JWTAuth]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        private readonly PrivacyPulseContext dataContext;
+
+        public UsersController(PrivacyPulseContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet("{id}/profile")]
+        public async Task<ActionResult<ProfileModel>> Get(int id)
+        {
+            var result = TryGetUserId(out var userId);
+
+            if (!result) return Unauthorized();
+
+            var user = await dataContext.Users.Include(x => x.Friends).FirstOrDefaultAsync(x => x.Id == id);.
+
+            if (user == null) return NotFound();
+
+            if (id != userId && !user.Friends.Any(x  => x.FriendUserId == userId))
+            {
+                return Unauthorized();
+            }
+            
+            return Ok(new ProfileModel
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Biography = user.Biography,
+            });
+
+        }
+    }
+}
---
+++ PrivacyPulse-BACK/Models/ProfileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrivacyPulse_BACK.Models
+{
+    public class ProfileModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Biography { get; set; }
+    }
+}

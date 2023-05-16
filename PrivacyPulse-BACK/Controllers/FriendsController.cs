2a7514334216b6fce4b7ea5a75e3b2e733f96f53
CaseBody <42807313+CaseBody@users.noreply.github.com>
2023-05-16 13:58:55 +0200

    progres

--- PrivacyPulse-BACK/Controllers/FriendsController.cs
+++ PrivacyPulse-BACK/Controllers/FriendsController.cs
@@ -36,5 +36,32 @@ namespace PrivacyPulse_BACK.Controllers
                 Username = x.FriendUser.Username,
             }));
         }
+
+        [HttpGet("find")]
+        public async Task<ActionResult<FriendModel>> FindFriends(string? name)
+        {
+            var result = TryGetUserId(out var userId);  
+
+            if (!result) return Unauthorized();
+
+            var user = await dataContext.Users.Include(x => x.Friends).FirstAsync(x => x.Id == userId);
+
+            var query = dataContext.Users.Where(x => x.Id != user.Id);
+
+            if (name != "" && name != null)
+            {
+                query = query.Where(x => x.Username.ToLower().Contains(name.ToLower()));
+            }
+
+            var users = await query.Take(10).ToListAsync();
+
+            users = users.Where(x => !user.Friends.Any(y => y.FriendUserId == x.Id)).ToList();
+
+            return Ok(users.Select(x => new
+            {
+                Id = x.Id,
+                Username = x.Username
+            }));
+        }
     }
 }

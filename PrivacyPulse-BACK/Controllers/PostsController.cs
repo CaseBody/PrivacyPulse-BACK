9875d0d22710c405357ab744139d6a2b34af0f5f
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-09 10:46:07 +0200

    startup controller

---
+++ PrivacyPulse-BACK/Controllers/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using CampingAPI.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using PrivacyPulse_BACK.Attributes;
+
+namespace PrivacyPulse_BACK.Controllers
+{
+    [Route("api/[controller]")]
+    [JWTAuth]
+    [ApiController]
+    public class PostsController : BaseController
+    {
+        private readonly PrivacyPulseContext dataContext;
+
+        public PostsController(PrivacyPulseContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+    }
+}

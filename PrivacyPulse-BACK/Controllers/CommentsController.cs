65f233a7f03113100fd018ea096fc5f0111cca7a
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-20 09:05:48 +0200

    add comment funtion

---
+++ PrivacyPulse-BACK/Controllers/CommentsController.cs
@@ -0,0 +1,45 @@
+﻿using CampingAPI.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrivacyPulse_BACK.Attributes;
+using PrivacyPulse_BACK.Constants;
+using PrivacyPulse_BACK.Entities;
+using PrivacyPulse_BACK.Models;
+
+namespace PrivacyPulse_BACK.Controllers
+{
+    [Route("api/[controller]")]
+    [JWTAuth]
+    [ApiController]
+    public class CommentsController : BaseController
+    {
+        private readonly PrivacyPulseContext dataContext;
+
+        public CommentsController(PrivacyPulseContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [Route("/api/comment/add")]
+        [HttpPost()]
+        public async Task<ActionResult> Create([FromForm] CommentModel commentData)
+        {
+            var result = TryGetUserId(out var userId);
+
+            if (!result) return Unauthorized();
+
+            var selectedPost = await dataContext.Posts.FirstOrDefaultAsync(p => p.Id == commentData.Id);
+
+            if (selectedPost == null) return NotFound();
+
+            var comment = new Comment
+            {
+                UserId = (int)userId,
+                PostId = selectedPost.Id,
+                Body = commentData.Body,
+            };
+
+            return Ok(comment);
+        }
+    }
+}
---
+++ PrivacyPulse-BACK/Models/CommentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PrivacyPulse_BACK.Models
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+    }
+}
